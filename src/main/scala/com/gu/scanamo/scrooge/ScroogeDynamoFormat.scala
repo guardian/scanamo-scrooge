abd5177ee0a5f698b62761e268fd1643aa2e4f29
Philip Wills <philip.wills@guardian.co.uk>
2016-07-07 15:52:50 +0100

    Added support for ThriftStruct

--- src/main/scala/com/gu/scanamo/scrooge/ScroogeDynamoFormat.scala
+++ src/main/scala/com/gu/scanamo/scrooge/ScroogeDynamoFormat.scala
@@ -1,12 +1,13 @@
 package com.gu.scanamo.scrooge
 
 import com.gu.scanamo.DynamoFormat
-import com.twitter.scrooge.ThriftEnum
+import com.twitter.scrooge.{ThriftEnum, ThriftStruct}
 
 import scala.language.experimental.macros
 
 object ScroogeDynamoFormat {
   implicit def scroogeScanamoEnumFormat[T <: ThriftEnum]: DynamoFormat[T] = macro ScroogeDynamoFormatMacro.enumMacro[T]
+  implicit def scroogeScanamoStructFormat[T <: ThriftStruct]: DynamoFormat[T] = macro ScroogeDynamoFormatMacro.structMacro[T]
 }
 
 import scala.reflect.macros.blackbox
@@ -26,6 +27,57 @@ class ScroogeDynamoFormatMacro(val c: blackbox.Context) {
       com.gu.scanamo.DynamoFormat.xmap[$A, String](
         (x) => _root_.cats.data.Xor.fromOption($valueOf(x), com.gu.scanamo.error.TypeCoercionError(throw new IllegalArgumentException(x + " is not a valid " + $typeName))))(
         _.name)
+    """
+  }
+
+  def structMacro[T: c.WeakTypeTag]: Tree = {
+    val A = weakTypeOf[T]
+
+    val apply = A.companion.member(TermName("apply")) match {
+      case symbol if symbol.isMethod && symbol.asMethod.paramLists.size == 1 => symbol.asMethod
+      case _ => c.abort(c.enclosingPosition, "Not a valid Scrooge class: could not find the companion object's apply method")
+    }
+
+    val params = apply.paramLists.head.zipWithIndex.map { case (param, i) =>
+      val name = param.name
+      val tpe = param.typeSignature
+      val fresh = c.freshName(name).toTermName
+      val termName = name.toTermName
+
+      // Note the use of shapeless `Lazy` to work around a problem with diverging implicits.
+      // If you try to summon an implicit for heavily nested type, e.g. `Decoder[Option[Seq[String]]]` then the compiler sometimes gives up.
+      // Wrapping with `Lazy` fixes this issue.
+      val reader =
+        q"""
+          val $fresh: cats.data.Validated[com.gu.scanamo.error.InvalidPropertiesError, $tpe] = {
+            val format = _root_.scala.Predef.implicitly[_root_.shapeless.Lazy[_root_.com.gu.scanamo.DynamoFormat[$tpe]]].value
+            val possibleValue = collection.convert.WrapAsScala.mapAsScalaMap(av.getM).get(${termName.toString}).map(format.read).orElse(format.default.map(cats.data.Xor.right))
+            val validatedValue = possibleValue.getOrElse(cats.data.Xor.left[com.gu.scanamo.error.DynamoReadError, $tpe](com.gu.scanamo.error.MissingProperty))
+            validatedValue.leftMap(e => com.gu.scanamo.error.InvalidPropertiesError(cats.data.NonEmptyList(com.gu.scanamo.error.PropertyReadError(${termName.toString}, e)))).toValidated
+          }
+          """
+      val writer = q"""${termName.toString} -> _root_.scala.Predef.implicitly[_root_.shapeless.Lazy[_root_.com.gu.scanamo.DynamoFormat[$tpe]]].value.write(t.$termName)"""
+      val freshSuccesful = q"""$fresh.toOption.get"""
+      (writer, reader, fresh, freshSuccesful)
+    }
+
+    q"""
+      new com.gu.scanamo.DynamoFormat[$A] {
+        def read(av: com.amazonaws.services.dynamodbv2.model.AttributeValue): cats.data.Xor[com.gu.scanamo.error.DynamoReadError, $A] = {
+          ..${params.map(_._2)}
+          List(..${params.map(_._3)}).reduce(_.product(_)).toXor.map(_ =>
+            ${apply.asMethod}(
+              ..${params.map(_._4)}
+            )
+          )
+        }
+        def write(t: $A): com.amazonaws.services.dynamodbv2.model.AttributeValue =
+          new com.amazonaws.services.dynamodbv2.model.AttributeValue().withM(collection.convert.WrapAsJava.mapAsJavaMap(
+            Seq(..${params.map(_._1)}).foldLeft(Map.empty[String, com.amazonaws.services.dynamodbv2.model.AttributeValue])((m,p) =>
+              m + p
+            )
+          ))
+      }
     """
   }
 }
--- src/test/scala/com/gu/scanamo/scrooge/ScroogeDynamoFormatTest.scala
+++ src/test/scala/com/gu/scanamo/scrooge/ScroogeDynamoFormatTest.scala
@@ -1,10 +1,10 @@
 package com.gu.scanamo.scrooge
 
-
 import cats.data.Xor
+import com.gu.contentatom.thrift.{ChangeRecord, User}
 import com.gu.contentatom.thrift.atom.media.AssetType
+import com.gu.scanamo.DynamoFormat
 import org.scalatest.{FunSuite, Matchers}
-
 
 class ScroogeDynamoFormatTest extends FunSuite with Matchers {
 
@@ -14,4 +14,9 @@ class ScroogeDynamoFormatTest extends FunSuite with Matchers {
     format.read(format.write(AssetType.Video)) should be(Xor.right(AssetType.Video))
   }
 
+  test("testScroogeScanamoStructFormat") {
+    import ScroogeDynamoFormat._
+    val changeRecord = ChangeRecord(1L, Some(User("email", Some("f"), None)))
+    DynamoFormat[ChangeRecord].read(DynamoFormat[ChangeRecord].write(changeRecord)) should be(Xor.right(changeRecord))
+  }
 }
